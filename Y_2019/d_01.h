468fa2c1e58d1cae19d6e17f2e0993c2f034e633
Thomas Gale <gale.thomasjames@gmail.com>
2019-12-01 10:36:30 +0000

    2019 day 1 part 1 complete

--- Y_2019/d_01.h
+++ Y_2019/d_01.h
@@ -4,6 +4,16 @@
 
 namespace aoc::y2019::d01 {	
 	void calculate(std::istream& input) {
-		std::cout << "TODO.\n";
+		std::cout << "The Tyranny of the Rocket Equation\n";
+		std::vector<std::string> input_strings = aoc::read_input(input);
+
+		int total_fuel = std::accumulate(input_strings.begin(), input_strings.end(), 0, [](const int& total_fuel, const std::string& mass_str) {
+			int mass = std::stoi(mass_str);
+			return total_fuel + ((mass / 3) - 2);
+			}
+		);
+
+		std::cout << "1. Fuel requirements:\n";
+		std::cout << total_fuel << "\n";
 	}
 }
