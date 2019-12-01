5f0a1040ea05e43416bcfc32d2c73eb85a856596
Thomas Gale <gale.thomasjames@gmail.com>
2019-12-01 11:17:29 +0000

    Day 1 part 2 complete.

--- Y_2019/d_01.h
+++ Y_2019/d_01.h
@@ -3,17 +3,39 @@
 #include <set>
 
 namespace aoc::y2019::d01 {	
+
+	int computeFuelToMoveMass(int mass)
+	{
+		return std::max(0, (mass / 3) - 2);
+	}
+
+	int computeFuelToMoveFuel(int fuel) {
+		if (fuel <= 0) return 0;
+		return fuel + computeFuelToMoveFuel(computeFuelToMoveMass(fuel));
+	}
+
 	void calculate(std::istream& input) {
 		std::cout << "The Tyranny of the Rocket Equation\n";
 		std::vector<std::string> input_strings = aoc::read_input(input);
-
+		
+		// Part 1.
 		int total_fuel = std::accumulate(input_strings.begin(), input_strings.end(), 0, [](const int& total_fuel, const std::string& mass_str) {
 			int mass = std::stoi(mass_str);
-			return total_fuel + ((mass / 3) - 2);
+			return total_fuel + computeFuelToMoveMass(mass);
 			}
 		);
 
-		std::cout << "1. Fuel requirements:\n";
+		std::cout << "1. Basic fuel requirements:\n";
+		std::cout << total_fuel << "\n";
+
+		// Part 2.
+		total_fuel = std::accumulate(input_strings.begin(), input_strings.end(), 0, [](const int& total_fuel, const std::string& mass_str) {
+			int mass = std::stoi(mass_str);
+			return total_fuel + computeFuelToMoveFuel(computeFuelToMoveMass(mass));
+			}
+		);
+
+		std::cout << "2. Sum of fuel requirements:\n";
 		std::cout << total_fuel << "\n";
 	}
 }
