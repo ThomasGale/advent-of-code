f72db546a0bdbdbad9fbf958053aab3e6e1f227c
Thomas Gale <gale.thomasjames@gmail.com>
2019-12-02 09:09:56 +0000

    2019 day 2 part 2
    Just did brute search over 10,000 options.

--- Y_2019/d_02.h
+++ Y_2019/d_02.h
@@ -5,6 +5,36 @@
 
 namespace aoc::y2019::d02 {
 
+	int RunProgram(std::vector<int>& programState)
+	{
+		int pProg = 0;
+		int opCode = 0;
+		int pI1 = 0;
+		int pI2 = 0;
+		int pOut = 0;
+
+		while (true) {
+			opCode = programState[pProg++];
+			if (opCode == 99) break;
+			pI1 = programState[pProg++];
+			pI2 = programState[pProg++];
+			pOut = programState[pProg++];
+
+			switch (opCode)
+			{
+			case 1:
+				programState[pOut] = programState[pI1] + programState[pI2];
+				break;
+			case 2:
+				programState[pOut] = programState[pI1] * programState[pI2];
+				break;
+			default:
+				throw std::runtime_error("Unrecognised opcode");
+			}
+		}
+		return programState[0];
+	}
+
 	void calculate(std::istream& input) {
 		std::cout << "--- Day 2: 1202 Program Alarm ---\n";
 		std::string input_str(std::istreambuf_iterator<char>(input), {});
@@ -17,37 +47,37 @@ namespace aoc::y2019::d02 {
 		}
 
 		// Part 1.
-		int pProg = 0;
-		int opCode = 0;
-		int pI1 = 0;
-		int pI2 = 0;
-		int pOut = 0;
 
 		// Setup - part of puzzle.
-		inputProgram[1] = 12;
-		inputProgram[2] = 2;
+		std::vector<int> part1ProgramState(inputProgram);
+		part1ProgramState[1] = 12;
+		part1ProgramState[2] = 2;
 
-		while (true) {
-			opCode = inputProgram[pProg++];
-			if (opCode == 99) break;
-			pI1 = inputProgram[pProg++];
-			pI2 = inputProgram[pProg++];
-			pOut = inputProgram[pProg++];
+		int output = RunProgram(part1ProgramState);
 
-			switch (opCode)
-			{
-			case 1:
-				inputProgram[pOut] = inputProgram[pI1] + inputProgram[pI2];
-				break;
-			case 2:
-				inputProgram[pOut] = inputProgram[pI1] * inputProgram[pI2];
-				break;
-			default:
-				throw std::runtime_error("Unrecognised opcode");
+		std::cout << "1. Program Output at Position 0:\n";
+		std::cout << output << "\n";
+
+		// Part 2.
+		// Brute search 
+		int noun = 0;
+		int verb = 0;
+		for (noun = 0; noun < 100; ++noun) {
+			for (verb = 0; verb < 100; ++verb) {
+				std::vector<int> part2ProgramState(inputProgram);
+				part2ProgramState[1] = noun;
+				part2ProgramState[2] = verb;
+				int output = RunProgram(part2ProgramState);
+				if (output == 19690720)
+				{
+					std::cout << "2. Noun and Verb Found:\n";
+					std::cout << noun << " and " << verb << "\n";
+					std::cout << "Ouput: " << 100 * noun + verb << "\n";
+				}
 			}
 		}
 
-		std::cout << "1. Program Output at Position 0:\n";
-		std::cout << inputProgram[0] << "\n";
+
+
 	}
 }
