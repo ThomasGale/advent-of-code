2d8c19b867050d47487eb3209f72b8ff5de8824f
Thomas Gale <gale.thomasjames@gmail.com>
2019-12-04 09:11:16 +0000

    2019 Day 4 part 1 complete

--- Y_2019/d_04.h
+++ Y_2019/d_04.h
@@ -4,15 +4,33 @@
 
 namespace aoc::y2019::d04 {	
 	void calculate(std::istream& input) {
-		std::cout << " Day xx \n";
+		std::cout << "--- Day 4: Secure Container ---\n";
 
-		//std::string input_str(std::istreambuf_iterator<char>(input), {});
-		//std::stringstream ss(input_str);
+		std::vector<int> validPasswords;
 
-		//std::vector<std::string> input_strings = aoc::utils::read_input(input);
+		int validCount = 0;
+		for (int pw = 124075; pw <= 580769l; ++pw) {
+			auto pwStr = std::to_string(pw);
 
-		std::cout << "1. ... :\n";
-		std::cout << "" << "\n";
+			bool incrOk = true;
+			for (auto i = 0; i < pwStr.size() - 1; ++i) {
+				if (pwStr[i + 1] < pwStr[i]) incrOk = false;
+			}
+
+			bool dup = false;
+			for (auto i = 0; i < pwStr.size() - 1; ++i) {
+				if (pwStr[i] == pwStr[i + 1]) {
+					dup = true;
+				}
+			}
+			if (incrOk && dup) {
+				++validCount;
+				validPasswords.push_back(pw);
+			}
+		}
+
+		std::cout << "1. Passwords that meet criteria:\n";
+		std::cout << validCount << "\n";
 
 		std::cout << "2. ... :\n";
 		std::cout << "" << "\n";
