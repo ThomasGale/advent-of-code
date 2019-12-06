3c56f0caa8646d28c7b5bd4dca63fec555074984
Thomas Gale <gale.thomasjames@gmail.com>
2019-12-06 10:09:32 +0000

    2019 day 6 part 2 complete
    Bit hacky - needs a tidy and removal of duplicate code.

--- Y_2019/d_06.h
+++ Y_2019/d_06.h
@@ -3,6 +3,30 @@
 
 namespace aoc::y2019::d06 {
 
+	int computeDistance(std::map<std::string, std::string>& orbitMap, const std::string& start, const std::string& end) {
+		int distance = 0;
+		std::string currentEntity = start;
+		while (true) {
+			if ((currentEntity == "COM") || (currentEntity == end)) break;
+			++distance;
+			currentEntity = orbitMap[currentEntity];
+		}
+		return distance;
+	}
+
+	// Don't call with COM
+	std::set<std::string> computeParents(std::map<std::string, std::string>& orbitMap, const std::string& entity) {
+		std::set<std::string> parents;
+		std::string currentEntity = entity;
+		while (true) {
+			std::string currentParent = orbitMap[currentEntity];
+			parents.insert(currentParent);
+			if (currentParent == "COM") break;
+			currentEntity = currentParent;
+		}
+		return parents;
+	}
+
 	void calculate(std::istream& input) {
 		std::cout << "--- Day 6: Universal Orbit Map ---\n";
 		std::vector<std::string> inputStrs = aoc::utils::read_input(input);
@@ -12,6 +36,7 @@ namespace aoc::y2019::d06 {
 			orbitMap[orbitsStr[1]] = orbitsStr[0];
 		}
 
+		// Part 1.
 		int totalOrbits = 0;
 		for (const auto& orbit : orbitMap) {
 			++totalOrbits;
@@ -24,5 +49,24 @@ namespace aoc::y2019::d06 {
 
 		std::cout << "1. Total number of direct and indirect orbits :\n";
 		std::cout << totalOrbits << "\n";
+
+		// Part 2.
+		auto youParents = computeParents(orbitMap, "YOU");
+		auto sanParents = computeParents(orbitMap, "SAN");
+		
+		std::set<std::string> commonParents;
+		std::set_intersection(youParents.begin(), youParents.end(), sanParents.begin(), sanParents.end(), std::inserter(commonParents, commonParents.begin()));
+		
+		std::vector<int> distances;
+		// Comptute total distance from YOU and SAN to common parent
+		for (auto& parent : commonParents) {
+			int distance = computeDistance(orbitMap, "YOU", parent) - 1;
+			distance += computeDistance(orbitMap, "SAN", parent) - 1;
+			distances.push_back(distance);
+		}
+
+		// Take min.
+		std::cout << "3. Min distance between YOU and SAN:\n";
+		std::cout << *std::min_element(distances.begin(), distances.end()) << "\n";
 	}
 }
--- Y_2019/default.h
+++ Y_2019/default.h
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <numeric>
 #include <optional>
 #include <set>
