484aacee5ef0297d21d48a53305a99e43fe2f2f0
Thomas Gale <thomas.gale@autodesk.com>
2019-12-13 19:04:35 +0000

    Adding some experimental PDCurses panel stuff.

--- Y_2019/d_14.h
+++ Y_2019/d_14.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "default.h"
 #include "curses.h"
+#include "panel.h"
 
 namespace aoc::y2019::d14 {
 	void calculate(std::istream& input) {
@@ -20,12 +21,29 @@ namespace aoc::y2019::d14 {
 		keypad(stdscr, true);
 		erase();
 
-		int y, x;
-		for (y = 0; y < LINES - 1; y++)
-			for (x = 0; x < COLS; x++)
-				printw("%d", (y + x) % 10);
+		//int y, x;
+		//for (y = 0; y < LINES - 1; y++)
+		//	for (x = 0; x < COLS; x++)
+		//		printw("%d", (y + x) % 10);
 
-		
+		WINDOW* subWin = newwin(15, 15, 0, 0);
+		PANEL* p = new_panel(subWin);
+		set_panel_userptr(p, "p");
+
+		char num = *((char*)p->user + 1);
+		int y, x, maxy, maxx;
+
+		box(subWin, 0, 0);
+		mvwprintw(subWin, 1, 1, "-pan%c-", num);
+		getmaxyx(subWin, maxy, maxx);
+
+		for (y = 2; y < maxy - 1; y++)
+			for (x = 1; x < maxx - 1; x++)
+				mvwaddch(subWin, y, x, num);
+	
+		update_panels();
+		doupdate();
+
 		napms(5000);
 		endwin();
 
