0e06a45b7b11c808a54a92fe8a715cf67bf23747
Thomas Gale <gale.thomasjames@gmail.com>
2019-12-15 23:09:56 +0000

    Day 15 part 2 complete
    This is not code I'm proud of, needs a serious tidy - hacked together.

--- Y_2019/d_15.h
+++ Y_2019/d_15.h
@@ -37,12 +37,14 @@ namespace aoc::y2019::d15 {
 		Loc droidPos(windowX / 2, windowY / 2);
 		Ori droidOri({ 0, -1 }, { 1, 0 }); // Coordinate system left-handed, top left (like in previous examples).
 		std::map<Loc, MapState> map{ {droidPos, MapState(0, 0)} }; // 0 clear, 1 wall, 2 goal.
+		std::set<Loc> unexplored;
 		aoc::utils::BasicWindow window(windowX, windowY);
 		Loc target;
+		int distanceToTarget = 0; // Part 1.
 		int currStatus = 0;
-		int currDistance = 0;
+		int currDist = 0;
 
-		// Search for tank 
+		// Exhausive Exploration.
 		while (true) {
 
 			// Move droid in current direction
@@ -53,7 +55,7 @@ namespace aoc::y2019::d15 {
 
 			// Movement status update.
 			if (currStatus == 0) { // Wall hit
-				map.insert({ {getLX(droidPos) + getLX(droidOri.first), getLY(droidPos) + getLY(droidOri.first)}, MapState(1, currDistance) }); // Record wall (in front of droid)
+				map.insert({ {getLX(droidPos) + getLX(droidOri.first), getLY(droidPos) + getLY(droidOri.first)}, MapState(1, currDist) }); // Record wall (in front of droid)
 			}
 			else { // Droid could move forward in direction it was facing
 				if (currStatus == 1) {
@@ -61,34 +63,47 @@ namespace aoc::y2019::d15 {
 					setLY(droidPos, getLY(droidPos) + getLY(droidOri.first));
 					auto currMap = map.find(droidPos);
 					if (currMap == map.end()) {
-						++currDistance;
-						map.insert({ droidPos, MapState(0, currDistance) }); // Record free space
+						++currDist;
+						map.insert({ droidPos, MapState(0, currDist) }); // Record free space
 					}
 					else {
-						currDistance = currMap->second.Distance;
+						currDist = currMap->second.Distance;
 					}
 				}
 				else if (currStatus == 2) { // Oxygen found!
 					setLX(droidPos, getLX(droidPos) + getLX(droidOri.first));
 					setLY(droidPos, getLY(droidPos) + getLY(droidOri.first));
-					++currDistance;
-					map.insert({ droidPos, MapState(2, currDistance) }); // Record oxygen location.
+					++currDist;
+					map.insert({ droidPos, MapState(2, currDist) }); // Record oxygen location.
 					target = droidPos;
-					break;
+					distanceToTarget = currDist;
 				} else { throw std::runtime_error("Unexpected status."); };
-
 			}
 
-			auto leftDroid = map.find({ getLX(droidPos) - getLX(droidOri.second), getLY(droidPos) - getLY(droidOri.second) });
-			auto rightDroid = map.find({ getLX(droidPos) + getLX(droidOri.second), getLY(droidPos) + getLY(droidOri.second) });
-			auto frontDroid = map.find({ getLX(droidPos) + getLX(droidOri.first), getLY(droidPos) + getLY(droidOri.first) });
+			// Get information about surroundings.
+			Loc left = { getLX(droidPos) - getLX(droidOri.second), getLY(droidPos) - getLY(droidOri.second) };
+			Loc right = { getLX(droidPos) + getLX(droidOri.second), getLY(droidPos) + getLY(droidOri.second) };
+			Loc front = { getLX(droidPos) + getLX(droidOri.first), getLY(droidPos) + getLY(droidOri.first) };
 
-			// Rather Hacky turn left algorithm.
+			auto leftDroid = map.find(left);
+			auto rightDroid = map.find(right);
+			auto frontDroid = map.find(front);
+
+			// Update unexplored set;
+			unexplored.erase(droidPos);
+			if (leftDroid != map.end()) unexplored.erase(leftDroid->first);
+			else unexplored.insert(left);
+			if (rightDroid != map.end()) unexplored.erase(rightDroid->first);
+			else unexplored.insert(right);
+			if (frontDroid != map.end()) unexplored.erase(frontDroid->first);
+			else unexplored.insert(front);
+
+			// Rather hacky turn left maze walk algorithm.
 			if (frontDroid == map.end()) { // Explore (no rotate, move forward)
-			} else if (leftDroid == map.end()) { // Explore
-				droidOri = rotateLeft(droidOri);
+			} else if (leftDroid == map.end()) {
+				droidOri = rotateLeft(droidOri); // Explore unknown
 			} else if (rightDroid == map.end()) {
-				droidOri = rotateRight(droidOri); // Explore
+				droidOri = rotateRight(droidOri); // Explore unknown
 			} else { // We know all 3 sides.
 				if (rightDroid->second.Type == 0 && frontDroid->second.Type == 1 && leftDroid->second.Type == 1) { // Only turn right at bends in passage to the right.
 					droidOri = rotateRight(droidOri);
@@ -112,12 +127,69 @@ namespace aoc::y2019::d15 {
 			// Draw droid
 			window.SetChar(getLX(droidPos), getLY(droidPos), 'D');
 			window.Update();
+
+			// Check if fully explored
+			if (unexplored.size() == 0) break;
 		}
 
 		std::cout << "1. What is the fewest number of movement commands to oxygen system:\n";
-		std::cout << currDistance << "\n";
+		std::cout << distanceToTarget << "\n";
+		
+		// Part 2. Starting from the oxygen location. Walk out the edges of the graph of connections in the maze corridors.
+		std::map<Loc, int> stepsFromOxygen;
+		std::queue<std::pair<Loc, int>> unvisitedWithDist;
+		unvisitedWithDist.push({ target, 0 });
+		Loc currLoc;
+		currDist = 0;
 
-		std::cout << "2. ... :\n";
-		std::cout << "" << "\n";
+		while (true) { // Not a pretty loop :| 
+			auto currentLocationAndDist = unvisitedWithDist.front();
+			unvisitedWithDist.pop();
+			currLoc = currentLocationAndDist.first;
+			currDist = currentLocationAndDist.second;
+
+			Loc left = { getLX(currLoc) - 1, getLY(currLoc) };
+			Loc right = { getLX(currLoc) + 1, getLY(currLoc) };
+			Loc up = { getLX(currLoc), getLY(currLoc) - 1 };
+			Loc down = { getLX(currLoc), getLY(currLoc) + 1 };
+
+			auto leftOnMap = map.find(left);
+			auto rightOnMap = map.find(right);
+			auto upOnMap = map.find(up);
+			auto downOnMap = map.find(down);
+
+			auto leftOxy = stepsFromOxygen.find(left);
+			auto rightOxy = stepsFromOxygen.find(right);
+			auto upOxy = stepsFromOxygen.find(up);
+			auto downOxy = stepsFromOxygen.find(down);
+
+			if (leftOnMap != map.end() && leftOnMap->second.Type == 0 && leftOxy == stepsFromOxygen.end()) {
+				stepsFromOxygen.insert({ left, currDist + 1 });
+				unvisitedWithDist.push({ left, currDist + 1 });
+			}
+			if (rightOnMap != map.end() && rightOnMap->second.Type == 0 && rightOxy == stepsFromOxygen.end()) {
+				stepsFromOxygen.insert({ right, currDist + 1 });
+				unvisitedWithDist.push({ right, currDist + 1 });
+			}
+			if (upOnMap != map.end() && upOnMap->second.Type == 0 && upOxy == stepsFromOxygen.end()) {
+				stepsFromOxygen.insert({ up, currDist + 1 });
+				unvisitedWithDist.push({ up, currDist + 1 });
+			}
+			if (downOnMap != map.end() && downOnMap->second.Type == 0 && downOxy == stepsFromOxygen.end()) {
+				stepsFromOxygen.insert({ down, currDist + 1 });
+				unvisitedWithDist.push({ down, currDist + 1 });
+			}
+
+			if (unvisitedWithDist.empty()) break; // Onces all unvisited nodes are processed, that's the end.
+		}
+
+		// Find length of deepest chain.
+		int maxSteps = 0;
+		for (auto& step : stepsFromOxygen) {
+			if (step.second > maxSteps) maxSteps = step.second;
+		}
+
+		std::cout << "2. How many minutes will it take to fill with oxygen:\n";
+		std::cout << maxSteps  << "\n";
 	}
 }
