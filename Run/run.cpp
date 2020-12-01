f369d2a3ba24744b1e3af3dce4ede4bc9bedcc91
Thomas Gale <gale.thomasjames@gmail.com>
2020-12-01 18:42:42 +0000

    WIP: cleaning up run.

--- Run/run.cpp
+++ Run/run.cpp
@@ -1,5 +1,11 @@
-﻿#include "run.h"
+﻿#include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
 
+#include "../Y_2018/y_2018.h"
+#include "../Y_2019/y_2019.h"
 
 int main(int argc, char** argv)
 {
