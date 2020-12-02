9e3b11404fc6feaf135805b4de65b2fccdb76ae4
Thomas Gale <gale.thomasjames@gmail.com>
2020-12-02 01:44:19 +0000

    Changing the file extensions.

--- src/solutions/y2020/RegisteredSolutions.hpp
+++ src/solutions/y2020/RegisteredSolutions.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
 #include "Solution.hpp"
-#include "d01.hpp"
+#include "d01.cu"
 
 namespace aoc {
 namespace y2020 {
 
 Solution::Registrar<y2020::d01> r01(2020, 01, "Combination something or other");
 
-}
+} // namespace y2020
 } // namespace aoc
